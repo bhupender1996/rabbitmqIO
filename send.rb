b4da81d38e99e7e54d3e3e1e847550cb5e46e6f3
bhupender1996 <100000bhupendra@gmail.com>
2018-06-10 17:31:58 +0530

    Update send.rb

--- send.rb
+++ send.rb
@@ -1,7 +1,7 @@
 require 'bunny'
 require 'json'
 
-b =Bunny.new('amqp://jtnipnty:oiyV4umiKwf_la6R-2DQ7rmOXDO3LCcT@otter.rmq.cloudamqp.com/jtnipnty')
+b =Bunny.new('URL')
 b.start
 ch=b.create_channel
 q=ch.queue('INPUT', durable: true)
